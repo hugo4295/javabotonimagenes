3fdaea2ce620c0a91dcc124d225938814e0a6fc6
hugo4295 <hugo4295@hugo4295-Inspiron-N5110>
2020-03-25 13:56:30 -0600

    practica de botones con imagenes

---
+++ src/imagenes/Imagenes.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imagenes;
+
+import java.awt.Dialog;
+import javax.accessibility.AccessibleState;
+import javax.swing.JDialog;
+
+/**
+ *
+ * @author hugo4295
+ */
+public class Imagenes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrmPrincipal frmPrincipal = new FrmPrincipal();
+        frmPrincipal.setVisible(true);
+        //frmPrincipal.setVisible(true);
+       
+    }
+    
+}
